fa6b1308e35acae822c1e3c42051a0588190d5a3
HeenalMewada <mewadaheenal304@gmail.com>
2026-03-31 17:42:25 +0530

    fix: downgrade vite to v5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,41 +2,44 @@ import React from "react";
 import "./Header.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
+
 function Header() {
+
   return (
     <div className="header">
 
       <h3>&lt;Heenal's Portfolio/&gt;</h3>
-      
 
 
 
-        {/* Links */}
-        <div className="navlink">
-          <a
-            href="https://github.com/HeenalMewada"
-            target="_blank"
-          >
-           < FaGithub/> GitHub
-          </a>
 
-          <a
-            href="www.linkedin.com/in/heenal-mewada-7562a5360"
-            target="_blank"
+      {/* Links */}
+      <div className="navlink">
+        <a
+          href="https://github.com/HeenalMewada"
+          target="_blank"
+        >
+          < FaGithub /> GitHub
+        </a>
 
-          >
-          <FaLinkedin/>  LinkedIn
-          </a>
+        <a
+          href="www.linkedin.com/in/heenal-mewada-7562a5360"
+          target="_blank"
 
-          <button className="btn-resume">
-  Resume
+        >
+          <FaLinkedin />  LinkedIn
+        </a>
+        <a href="./Heenal Mewada CV.pdf" target="_blank">
+          <button className="btn-resume" >
+            Resume
 
-</button>        
+          </button>
+        </a>
 
 
-</div>
       </div>
-    
+    </div>
+
 
   );
 }
